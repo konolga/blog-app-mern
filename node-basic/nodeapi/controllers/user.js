0950b620f3c2c4f02b980b4498148541d95a9349
Olga Kon <konolga@users.noreply.github.com>
2019-03-16 00:43:24 +0200

    delete user

--- node-basic/nodeapi/controllers/user.js
+++ node-basic/nodeapi/controllers/user.js
@@ -1,40 +1,73 @@
+const _ = require('lodash');
 const User = require("../models/user");
 
-exports.userById = (req, res, next, id)=>{
-    User.findById(id).exec((err, user)=>{
-if(err||!user){
-    return res.status(400).json({
-        error: "User not found"
-    })
-}
-req.profile = user //adds profile object in req with user info
-next();
+exports.userById = (req, res, next, id) => {
+    User.findById(id).exec((err, user) => {
+        if (err || !user) {
+            return res.status(400).json({
+                error: "User not found"
+            })
+        }
+        req.profile = user //adds profile object in req with user info
+        next();
     })
 };
 
-exports.hasAuthorization = (res, req, next)=>{
+exports.hasAuthorization = (res, req, next) => {
     const authorized = req.profile && req.auth && req.profile === req.auth._id
-    if(!authorized){
+    if (!authorized) {
         return res.status(403).json({
             error: "User is not authorized"
         })
     }
 };
 
-exports.allUsers = (req, res)=>{
-    User.find((err, users)=>{
-if(err){
-    return res.status(400).json({
-        error: err
-    })
-}
+exports.allUsers = (req, res) => {
+    User.find((err, users) => {
+        if (err) {
+            return res.status(400).json({
+                error: err
+            })
+        }
 
-res.json({users});
+        res.json({
+            users
+        });
     }).select("name email updated created");
 };
 
-exports.getUser = (req, res)=>{
+exports.getUser = (req, res) => {
     req.profile.hashed_password = undefined;
     req.profile.salt = undefined;
     return res.json(req.profile);
+};
+
+exports.updateUser = (req, res, next) => {
+    let user = req.profile;
+    user = _.extend(user, req.body) // extend mutate the source object
+    user.updated = Date.now();
+    user.save((err) => {
+        if (err) {
+            return res.status(400).json({
+                error: "You are not authorized!"
+            })
+        }
+
+        user.hashed_password = undefined;
+        user.salt = undefined;
+        res.json(user);
+    });
+};
+
+exports.deleteUser = (req, res, next) => {
+    let user = req.profile;
+    user.remove((err, user) => {
+        if (err) {
+            return res.status(400).json({
+                error: err
+            });
+        }
+
+       res.json({message: "User deleted"});
+    });
 };
--- node-basic/nodeapi/routes/user.js
+++ node-basic/nodeapi/routes/user.js
@@ -1,5 +1,11 @@
 const express = require("express");
-const {userById, allUsers, getUser} = require ("../controllers/user");
+const {
+    userById,
+    allUsers,
+    getUser,
+    updateUser,
+    deleteUser
+} = require ("../controllers/user");
 const { requireSignin } = require("../controllers/auth");
 
 const router = express.Router();
@@ -7,6 +13,8 @@ const router = express.Router();
 
 router.get("/users", allUsers);
 router.get("/user/:userId", requireSignin, getUser);
+router.put("/user/:userId", requireSignin, updateUser); //put for make changesn
+router.delete("/user/:userId", requireSignin, deleteUser); 
 
 //any route contating userId will execute first userById
 router.param("userId",userById)
