ef6f2746d620f05ca66fac7564dc630a6aa21ec8
Olga Kon <konolga@users.noreply.github.com>
2019-03-23 15:16:47 +0200

    signin and redirect to main page

--- react-front/src/MainRouter.js
+++ react-front/src/MainRouter.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import Home from './core/Home'
 import Signup from './user/Signup'
+import Signin from './user/Signin'
 
 
 const MainRouter =()=>(
     <div>
         <Switch>
             <Route path="/signup" component = {Signup} />
+            <Route path="/signin" component = {Signin} />
             <Route path="/" component = {Home} />
             
         </Switch>
