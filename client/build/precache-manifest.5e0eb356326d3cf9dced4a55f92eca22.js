bec4bfef47bf8ac989e18689211d253cea4f8199
konolga <olgakon.mail@gmail.com>
2019-06-08 17:27:54 +0300

    added build

---
+++ client/build/precache-manifest.5e0eb356326d3cf9dced4a55f92eca22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bcd611bb25a86c65da5ffdba61433f6",
+    "url": "/konolga/blog-app-mern/index.html"
+  },
+  {
+    "revision": "226db59e9505c523c9df",
+    "url": "/konolga/blog-app-mern/static/js/2.e76f0a22.chunk.js"
+  },
+  {
+    "revision": "ffedb60da32837c21ec2",
+    "url": "/konolga/blog-app-mern/static/js/main.4ead6b66.chunk.js"
+  },
+  {
+    "revision": "77c4b637120acc00282b",
+    "url": "/konolga/blog-app-mern/static/js/runtime~main.a5d698d0.js"
+  },
+  {
+    "revision": "d5f51047d8bd6327ec4a74361a7aae7f",
+    "url": "/konolga/blog-app-mern/static/media/avatar.d5f51047.jpg"
+  },
+  {
+    "revision": "e84edac588fecdae254bf220bf83971d",
+    "url": "/konolga/blog-app-mern/static/media/post-default.e84edac5.jpg"
+  }
+]);
